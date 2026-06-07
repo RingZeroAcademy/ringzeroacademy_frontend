506da854f27cd90085643e784037f6d13aaad2aa
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-06-07 17:48:47 +0300

    Add 'Logo' component.

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import styles from "./Logo.module.css";
+
+export default function Logo() {
+    return <a href="#" className={styles.logo}>
+        <div className={styles.logoIcon}></div>
+        <h3 className={styles.logoText}>RingZeroAcademy</h3>
+    </a>
+}
