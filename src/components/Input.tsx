a33bff5cf449b1a3d67afe6e1672aa5dbb624cea
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-06-10 09:56:05 +0300

    Add the 'secondaryText' utility class.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
 import "./Input.module.css";
 
 export default function Input({ placeholder }) {
-    return <input type="text" placeholder={placeholder} />
+    return <input type="text" placeholder={placeholder} className="secondaryText" />
 }
