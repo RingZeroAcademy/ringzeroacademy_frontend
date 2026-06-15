6b1f15d600c08ba589981227317d01bd9d47068f
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-06-15 21:47:03 +0300

    Make logo text cyan and remove logo icon.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import styles from "./Logo.module.css";
 
 export default function Logo() {
     return <a href="#" className={styles.logo}>
-        <div className={styles.logoIcon}></div>
+    {/*        <div className={styles.logoIcon}></div> */}
         <h3 className={styles.logoText}>RingZeroAcademy</h3>
     </a>
 }
