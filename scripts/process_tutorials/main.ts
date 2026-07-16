bf9909f7494e3af2a7ff8388e9ceb7aa98db0e25
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-07-16 20:29:08 +0300

    Save the processed html in the same directory as the original markdown files.

--- scripts/process_tutorials/main.ts
+++ scripts/process_tutorials/main.ts
@@ -3,16 +3,19 @@ import { execSync } from "child_process";
 import * as path from "path";
 import * as fs from "fs";
 
+/* Get the root directory of the repository containing all the frontend code.
+ * A temporary working directory will be created there, and later deleted after
+ * all the tutorials are processed.
+ */
 const repoRoot: string = execSync("git rev-parse --show-toplevel", { encoding: "utf-8" }).trim();
-
 const tempDir = path.join(repoRoot, "temp");
 fs.mkdirSync(tempDir);
 
-const tutorialsRepoDir: string = path.join(tempDir, "tutorials_repo");
-execSync(`git clone --depth 1 --branch main --single-branch git@github.com:RingZeroAcademy/tutorials.git ${tutorialsRepoDir}`);
+const tutorialContentDir: string = path.join(tempDir, "tutorials");
+execSync(`git clone --depth 1 --branch main --single-branch git@github.com:RingZeroAcademy/tutorials.git ${tutorialContentDir}`);
 
-const htmlTutorialsDir: string = path.join(tempDir, "html_tutorials");
-convertMarkdownToHtml(tutorialsRepoDir, htmlTutorialsDir);
+// Save the html files in the same directory as the original markdown files.
+convertMarkdownToHtml(tutorialContentDir, tutorialContentDir);
 
 
 
