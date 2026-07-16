ec4ab8ef6b06b28407dd244b70413ba39f8feb98
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-07-16 18:01:03 +0300

    Remove 'process_tutorials.sh'.

--- scripts/process_tutorials.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script assumes that its executed from the repository root.
-git clone --depth 1 --branch main --single-branch git@github.com:RingZeroAcademy/tutorials.git scripts/tutorials
-node scripts/process_tutorials.js
-rm -r scripts/tutorials
