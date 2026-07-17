cd3a0020d46d28297c28d12002e4f3fc190cc6c6
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-07-17 16:59:03 +0300

    Render the tutorial page to an html string.

---
+++ scripts/process_tutorials/renderTutorialsPage.tsx
@@ -0,0 +1,12 @@
+/*
+ * For every html file in 'targetDir':
+ *   - render the tutorials page with the html file's contents.
+ *   - Overwrite the original html file with the rendered html content.
+ */
+
+import TutorialPage from "../../src/TutorialPage/dist/TutorialPage.js";
+import React from "react"; // To avoid "ReferenceError: React is not defined".
+import { renderToString } from "react-dom/server";
+
+const output = renderToString(<TutorialPage tutorialHTML="Hello World" />);
+console.log(output);
