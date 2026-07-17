a22e20e39879a05355aa5b009f94d8dd5e02d925
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-07-17 12:14:29 +0300

    Add button components for normal links.

--- src/globalComponents/Buttons.tsx
+++ src/globalComponents/Buttons.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Buttons.module.css";
 
-export function PrimaryLinkButton({ text, to }) {
+export function PrimaryReactRouterLinkButton({ text, to }) {
     return <Link to={to} className={styles.primaryLinkButton} >{text}</Link>
 }
 
-export function SecondaryLinkButton({ text, to }) {
+export function SecondaryReactRouterLinkButton({ text, to }) {
     return <Link to={to} className={styles.secondaryLinkButton} >{text}</Link>
+}
+
+export function PrimaryNormalLinkButton({ text, to }) {
+    return <a className={styles.primaryLinkButton} href={to} >{text}</a>
+}
+
+export function SecondaryNormalLinkButton({ text, to }) {
+    return <a className={styles.secondaryLinkButton} href={to} >{text}</a>
 }
