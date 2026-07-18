0255a91352dd7ce3561ae3ba1838eaef0a5023ab
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-07-18 14:28:47 +0300

    Show 'git clone's output on the terminal.

--- scripts/process_tutorials/main.ts
+++ scripts/process_tutorials/main.ts
@@ -1,10 +1,11 @@
 import convertMarkdownToHtml from "./convert_markdown_to_html.ts";
+import insertTutorialsInTutorialPage from "processTutorialPage.tsx";
 import { execSync } from "child_process";
 import * as path from "path";
 import * as fs from "fs";
 
 /* Get the root directory of the repository containing all the frontend code.
- * A temporary working directory will be created there, and later deleted after
+ * A temporary working directory (`temp`) will be created there, and later deleted after
  * all the tutorials are processed.
  */
 const repoRoot: string = execSync("git rev-parse --show-toplevel", { encoding: "utf-8" }).trim();
@@ -12,12 +13,17 @@ const tempDir = path.join(repoRoot, "temp");
 fs.mkdirSync(tempDir);
 
 const tutorialContentDir: string = path.join(tempDir, "tutorials");
-execSync(`git clone --depth 1 --branch main --single-branch git@github.com:RingZeroAcademy/tutorials.git ${tutorialContentDir}`);
+execSync(
+    `git clone --depth 1 --branch main --single-branch git@github.com:RingZeroAcademy/tutorials.git ${tutorialContentDir}`,
+    { stdio: "inherit" }
+);
 
-// Save the html files in the same directory as the original markdown files.
 convertMarkdownToHtml(tutorialContentDir);
 
-
+/* Combine the tutorial page html with the tutorial html.
+ * Overwrites the initial tutorial files after processing all of them successfully.
+ */
+insertTutorialsInTutorialPage(tutorialContentDir);
 
 
 
